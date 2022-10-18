c00e231a60e592fa7daead032a182d720b5bf121
Morteza <mshojaei179@gmail.com>
2022-10-18 11:46:49 +0330

    implement create fund form

--- client/src/components/Protected.jsx
+++ client/src/components/Protected.jsx
@@ -1,7 +1,6 @@
 import { Navigate } from "react-router-dom";
 
 export const Protected = ({ children }) => {
-  console.log("ACCOUNT", localStorage.getItem("ACCOUNT"));
   if (localStorage.getItem("ACCOUNT") === null) return <Navigate to="/" />;
   return <div>{children}</div>;
 };
--- client/src/pages/CreateFund.jsx
+++ client/src/pages/CreateFund.jsx
@@ -1,3 +1,50 @@
+import { useState } from "react";
+
 export const CreateFund = () => {
-  return <div>Create a new Project</div>;
+  const [fund, setFund] = useState({
+    name: null,
+    goal: null,
+    unit: null,
+  });
+
+  function handler(field) {
+    return function (e) {
+      setFund((f) => ({ ...f, [field]: e.target.value }));
+    };
+  }
+  const { name, goal, unit } = fund;
+  const canSubmit = name && goal && unit;
+
+  return (
+    <div className="flex flex-col justify-center items-center w-96">
+      <h1 className="text-3xl font-bold  mb-16">Create The Project</h1>
+      <input
+        value={name}
+        onChange={handler("name")}
+        type="text"
+        placeholder="Project Name"
+        class="input input-bordered input-info w-full mb-4"
+      />
+
+      <input
+        value={goal}
+        onChange={handler("goal")}
+        type="number"
+        placeholder="Total Funding"
+        class="input input-bordered input-info w-full mb-4"
+      />
+
+      <input
+        value={unit}
+        onChange={handler("unit")}
+        type="number"
+        placeholder="Funding Unit"
+        class="input input-bordered input-info w-full mb-4"
+      />
+
+      <button disabled={!canSubmit} class="btn btn-active btn-accent w-full">
+        Submit
+      </button>
+    </div>
+  );
 };
